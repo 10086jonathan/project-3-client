472842d127e0de3df1f12f337b1c1ee72b187582
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-06 17:23:17 -0800

    adds page clients and updates styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
+
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
 import './App.css';
+
+import { Route, Switch, Redirect } from 'react-router-dom'
+
+// create three page components, login, signup, dashboard, home
+// define as a function component and export them
+// have them return a div with the text content of login, signup ... etc
+// import thme into app.js
+// import switch and route from react router and then define the various routes for all pages
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <p>project 3</p>
+        <main>
+          <Switch>
+            <Route exact path="/" render={props =>
+              <HomePage />
+              } />
+            <Route exact path="/dashboard" render={props =>
+              <DashboardPage />
+              } />
+            <Route exact path="/signup" render={props =>
+              <SignupPage />
+              } />
+            <Route exact path="/login" render={props =>
+              <LoginPage />
+              } />
+          </Switch>
+        </main>
       <Footer />
     </div>
   );
