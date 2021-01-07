c798296d4dcde7e0513ce3cf9d13beb6c5a0580c
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-06 18:29:33 -0800

    adds signup page functionality

--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -1,9 +1,71 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function SignupPage(props) {
+    const [formState, setFormState] = useState(getInitialFormState());
+
+    function getInitialFormState() {
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+        }
+    }
+
+    function handleChange(event) {
+        setFormState(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }));
+    };
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('submitted form data: ', formState)
+        // todo: make ajax request to sign user
+        setFormState(getInitialFormState());
+        
+        props.history.push('/dashboard');
+    };
+
+
     return (
-        <div>
-            SignupPage
+        <div className="Page">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        value={formState.firstName}
+                        onChange={handleChange}
+                        name="firstName"
+                        type="text"
+                    />
+                    <input
+                        value={formState.lastName}
+                        onChange={handleChange}
+                        name="lastName"
+                        type="text"
+                    />
+                    <input
+                        value={formState.email}
+                        onChange={handleChange}
+                        name="email"
+                        type="email"
+                    />
+                    <input
+                        value={formState.password}
+                        onChange={handleChange}
+                        name="password"
+                        type="password"
+                    />
+                    <button>Sign Up</button>
+                </div>
+            </form>
         </div>
     )
 };
 
 export default SignupPage;
+
+// create a signup form as a react controlled component
+// this means you neee
