efe2ccecbafccb567d149222c9636f9cf3273a9f
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-06 16:26:06 -0800

    intial changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 import './App.css';
+
+
+// create two components inside of component dir
+// header component and footer component - these will be in separare folder
+// have them return header element or footer element
+// text fragment of header or footer
+// export them and then import them into app
+
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>project 3</p>
+      <Footer />
     </div>
   );
 }
