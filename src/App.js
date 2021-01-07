223b035d0d958dbba10e7a0492fca91be5af89bf
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-06 18:24:16 -0800

    congigs route props

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ function App() {
               <DashboardPage />
               } />
             <Route exact path="/signup" render={props =>
-              <SignupPage />
+              <SignupPage {...props} />
               } />
             <Route exact path="/login" render={props =>
-              <LoginPage />
+              <LoginPage {...props} />
               } />
           </Switch>
         </main>
